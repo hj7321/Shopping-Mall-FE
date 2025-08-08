c8967508e216e3da5001dafb6e3ce83b26084147
Hyeonjin <oboki2001@naver.com>
2025-08-08 11:53:54 +0900

    [feat] 장바구니 페이지 데이터 연동 및 결제 정보 전달 로직 추가

--- src/page/PaymentPage/PaymentPage.js
+++ src/page/PaymentPage/PaymentPage.js
@@ -1,28 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./style/paymentPage.style.css";
+import { useNavigate } from "react-router";
+import OrderReceipt from "./component/OrderReceipt";
+import PaymentForm from "./component/PaymentForm";
+import { cc_expires_format } from "../../utils/number";
+import { createOrder } from "../../features/order/orderSlice";
 
 const PaymentPage = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const { orderNum } = useSelector((state) => state.order);
-  // const [cardValue, setCardValue] = useState({
-  //   cvc: "",
-  //   expiry: "",
-  //   focus: "",
-  //   name: "",
-  //   number: "",
-  // });
-  // const navigate = useNavigate();
-  // const [firstLoading, setFirstLoading] = useState(true);
-  // const [shipInfo, setShipInfo] = useState({
-  //   firstName: "",
-  //   lastName: "",
-  //   contact: "",
-  //   address: "",
-  //   city: "",
-  //   zip: "",
-  // });
+  const [cardValue, setCardValue] = useState({
+    cvc: "",
+    expiry: "",
+    focus: "",
+    name: "",
+    number: "",
+  });
+  const navigate = useNavigate();
+  const [firstLoading, setFirstLoading] = useState(true);
+  const [shipInfo, setShipInfo] = useState({
+    firstName: "",
+    lastName: "",
+    contact: "",
+    address: "",
+    city: "",
+    zip: "",
+  });
+  const cartList = useSelector((state) => state.cart.cartList);
+  const totalPrice = useSelector((state) => state.cart.totalPrice);
 
   useEffect(() => {
     // 오더번호를 받으면 어디로 갈까?
@@ -31,22 +38,49 @@ const PaymentPage = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     // 오더 생성하기
+    const { firstName, lastName, contact, address, city, zip } = shipInfo;
+    dispatch(
+      createOrder({
+        totalPrice,
+        shipTo: { address, city, zip },
+        contact: { firstName, lastName, contact },
+        orderList: cartList.map((item) => {
+          return {
+            productId: item.productId._id,
+            price: item.productId.price,
+            qty: item.qty,
+            size: item.size,
+          };
+        }),
+      })
+    );
   };
 
   const handleFormChange = (event) => {
     //shipInfo에 값 넣어주기
+    const { name, value } = event.target;
+    setShipInfo((prev) => ({ ...prev, [name]: value }));
   };
 
-  // const handlePaymentInfoChange = (event) => {
-  //   //카드정보 넣어주기
-  // };
+  const handlePaymentInfoChange = (event) => {
+    //카드정보 넣어주기
+    const { name, value } = event.target;
+    if (name === "expiry") {
+      let newValue = cc_expires_format(value);
+      setCardValue((prev) => ({ ...prev, [name]: newValue }));
+      return;
+    }
+    setCardValue((prev) => ({ ...prev, [name]: value }));
+  };
 
-  // const handleInputFocus = (e) => {
-  //   setCardValue({ ...cardValue, focus: e.target.name });
-  // };
-  // if (cartList?.length === 0) {
-  //   navigate("/cart");
-  // }// 주문할 아이템이 없다면 주문하기로 안넘어가게 막음
+  const handleInputFocus = (e) => {
+    setCardValue({ ...cardValue, focus: e.target.name });
+  };
+
+  if (cartList?.length === 0) {
+    navigate("/cart");
+  } // 주문할 아이템이 없다면 주문하기로 안넘어가게 막음
+
   return (
     <Container>
       <Row>
@@ -117,12 +151,16 @@ const PaymentPage = () => {
                   </Form.Group>
                 </Row>
                 <div className="mobile-receipt-area">
-                  {/* <OrderReceipt /> */}
+                  <OrderReceipt cartList={cartList} totalPrice={totalPrice} />
                 </div>
                 <div>
                   <h2 className="payment-title">결제 정보</h2>
                 </div>
-
+                <PaymentForm
+                  handleInputFocus={handleInputFocus}
+                  cardValue={cardValue}
+                  handlePaymentInfoChange={handlePaymentInfoChange}
+                />
                 <Button
                   variant="dark"
                   className="payment-button pay-button"
@@ -135,7 +173,7 @@ const PaymentPage = () => {
           </div>
         </Col>
         <Col lg={5} className="receipt-area">
-          {/* <OrderReceipt  /> */}
+          <OrderReceipt cartList={cartList} totalPrice={totalPrice} />
         </Col>
       </Row>
     </Container>
