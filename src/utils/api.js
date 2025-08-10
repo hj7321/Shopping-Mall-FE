3af0ac90c697b0e66aa1cb5a0c76d87f7f10c815
Hyeonjin <oboki2001@naver.com>
2025-08-11 01:32:24 +0900

    [refactor] api.js에 환경별 baseURL 추론 로직 주석 추가 및 코드 구조 개선

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,24 @@
 import axios from "axios";
+
+// function inferBaseURL() {
+//   const env = process.env.REACT_APP_LOCAL_BACKEND;
+//   // env가 있고 'localhost'가 아니면 그대로 사용
+//   if (env && !/localhost|127\.0\.0\.1/i.test(env)) return env;
+
+//   // 그렇지 않으면 현재 접속한 호스트(IP/도메인)로 구성
+//   const { protocol, hostname } = window.location; // ex) http://192.168.0.23:3000
+//   const port = process.env.REACT_APP_API_PORT || "5000";
+//   return `${protocol}//${hostname}:${port}/api`;
+// }
+
+// const api = axios.create({
+//   baseURL: inferBaseURL(),
+//   headers: {
+//     "Content-Type": "application/json",
+//     authorization: `Bearer ${sessionStorage.getItem("token")}`,
+//   },
+// });
+
 // 상황따라 주소 다름
 // const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
 const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
@@ -11,9 +31,7 @@ const api = axios.create({
     authorization: `Bearer ${sessionStorage.getItem("token")}`,
   },
 });
-/**
- * console.log all requests and responses
- */
+
 api.interceptors.request.use(
   (request) => {
     console.log("Starting Request", request);
