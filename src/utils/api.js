35cdf7b684cac7e59c3e7642b4d936cdb86608db
Hyeonjin <oboki2001@naver.com>
2025-08-11 17:39:27 +0900

    [docs] 필요 없는 console.log 삭제

--- src/utils/api.js
+++ src/utils/api.js
@@ -23,7 +23,6 @@ import axios from "axios";
 // const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
 const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
 // const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
-// console.log("proxy", BACKEND_PROXY);
 const api = axios.create({
   baseURL: PROD_BACKEND,
   headers: {
