dbe97ee033adf1902affbd95a54c59a4aeb30264
Hyeonjin <oboki2001@naver.com>
2025-08-13 20:18:18 +0900

    [refactor] SearchBox 컴포넌트 검색 로직 개선

--- src/common/component/SearchBox.js
+++ src/common/component/SearchBox.js
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { useSearchParams } from "react-router-dom";
 
-const SearchBox = ({ searchQuery, setSearchQuery, placeholder, field }) => {
-  const [query] = useSearchParams();
+const SearchBox = ({ query, navigate, placeholder, field }) => {
   const [keyword, setKeyword] = useState(query.get(field) || "");
 
   const onCheckEnter = (event) => {
     if (event.key === "Enter") {
-      setSearchQuery({ ...searchQuery, page: 1, [field]: event.target.value });
+      const newQuery = new URLSearchParams(query);
+      newQuery.set(field, event.target.value);
+      newQuery.set("page", 1);
+      navigate(`?${newQuery.toString()}`);
     }
   };
+
+  useEffect(() => {
+    setKeyword(query.get(field) || "");
+  }, [query, field]);
+
   return (
     <div className="search-box">
       <FontAwesomeIcon icon={faSearch} />
