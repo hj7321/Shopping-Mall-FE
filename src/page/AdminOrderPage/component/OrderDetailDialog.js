a5beae8f6abe0c87e5cbe87a07425c4296971e72
Hyeonjin <oboki2001@naver.com>
2025-08-13 20:24:46 +0900

    [delete] 불필요한 React import 제거

--- src/page/AdminOrderPage/component/OrderDetailDialog.js
+++ src/page/AdminOrderPage/component/OrderDetailDialog.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Form, Modal, Button, Col, Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { ORDER_STATUS } from "../../../constants/order.constants";
