2cbacf0613ac5e4ac2ec014fab74819a2f801ac2
Hyeonjin <oboki2001@naver.com>
2025-08-16 22:53:26 +0900

    [feat] CartProductCard 기능 확장 및 UI 개선 (체크박스, 상세 페이지 이동, 사이즈 변경 추가)

--- src/page/CartPage/component/CartProductCard.js
+++ src/page/CartPage/component/CartProductCard.js
@@ -3,22 +3,64 @@ import { Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDispatch } from "react-redux";
 import { currencyFormat } from "../../../utils/number";
-import { updateQty, deleteCartItem } from "../../../features/cart/cartSlice";
-const CartProductCard = ({ item }) => {
-  const dispatch = useDispatch();
+import {
+  updateCart,
+  deleteCartItem,
+  toggleCartItem,
+} from "../../../features/cart/cartSlice";
+import { useNavigate } from "react-router";
 
-  const handleQtyChange = (id, value) => {
-    dispatch(updateQty({ id, value }));
+const CartProductCard = ({ item, isChecked }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const goToProductDetail = () => {
+    navigate(`/product/${item.productId._id}`);
+  };
+
+  const handleSizeChange = (id, newSize) => {
+    dispatch(
+      updateCart({ id, qty: item.qty, size: newSize, oldSize: item.size })
+    );
+  };
+
+  const handleQtyChange = (id, newQty) => {
+    dispatch(
+      updateCart({ id, qty: newQty, size: item.size, oldQty: item.qty })
+    );
   };
 
   const deleteCart = (id) => {
-    dispatch(deleteCartItem(id));
+    const isConfirmed = window.confirm("정말로 삭제하시겠습니까?");
+    if (isConfirmed) {
+      dispatch(deleteCartItem([id]));
+    }
   };
 
+  const handleCheckboxChange = () => {
+    dispatch(toggleCartItem({ itemId: item._id }));
+  };
+
+  // 재고가 있는 사이즈만 필터링
+  const availableSizes = Object.entries(item.productId.stock)
+    .filter(([size, stock]) => stock > 0)
+    .map(([size]) => size);
+
   return (
-    <div className="p-[10px] bg-[#eeeeee] mb-[10px] rounded-[6px]">
+    <div className="p-[10px] bg-[#eeeeee] mb-[10px] rounded-[6px] relative">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleCheckboxChange}
+        className="absolute top-2 left-2 w-5 h-5"
+      />
       <Row>
-        <Col md={2} xs={12}>
+        <Col
+          md={2}
+          xs={12}
+          onClick={goToProductDetail}
+          style={{ cursor: "pointer" }}
+        >
           <img
             src={item.productId.image}
             width={112}
@@ -28,7 +70,13 @@ const CartProductCard = ({ item }) => {
         </Col>
         <Col md={10} xs={12}>
           <div className="relative display-flex space-between">
-            <h3 className="font-suit-700 text-[24px]">{item.productId.name}</h3>
+            <h3
+              className="font-suit-700 text-[24px]"
+              onClick={goToProductDetail}
+              style={{ cursor: "pointer" }}
+            >
+              {item.productId.name}
+            </h3>
             <button className="absolute right-[10px]">
               <FontAwesomeIcon
                 icon={faTrash}
@@ -38,9 +86,19 @@ const CartProductCard = ({ item }) => {
             </button>
           </div>
 
-          <div className="font-suit-400">
-            사이즈 :{" "}
-            <span className="font-suit-600">{item.size.toUpperCase()}</span>
+          <div className="font-suit-400 flex items-center gap-2 text-black">
+            <span>사이즈 : </span>
+            <select
+              value={item.size}
+              onChange={(e) => handleSizeChange(item._id, e.target.value)}
+              className="inline-block border rounded bg-white h-[24px] text-[12px] leading-[1] py-[2px] pr-[18px] pl-[6px] w-[80px] align-middle"
+            >
+              {availableSizes.map((sizeOption) => (
+                <option key={sizeOption} value={sizeOption}>
+                  {sizeOption.toUpperCase()}
+                </option>
+              ))}
+            </select>
           </div>
           <div className="font-suit-400">
             가격 :{" "}
@@ -49,7 +107,7 @@ const CartProductCard = ({ item }) => {
             </span>
           </div>
           <div className="font-suit-400 flex items-center gap-2 text-black">
-            <span>수량:</span>
+            <span>수량 : </span>
             <select
               value={item.qty ?? 1}
               onChange={(e) =>
