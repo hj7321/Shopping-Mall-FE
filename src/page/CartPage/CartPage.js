356f72ef0ec86ac35c6cc764c0517e18f60e562d
Hyeonjin <oboki2001@naver.com>
2025-08-16 22:52:59 +0900

    [feat] 장바구니 선택 기능 및 주문서 반영 로직 추가

--- src/page/CartPage/CartPage.js
+++ src/page/CartPage/CartPage.js
@@ -1,13 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import CartProductCard from "./component/CartProductCard";
 import OrderReceipt from "../PaymentPage/component/OrderReceipt";
 import "./style/cart.style.css";
-import { getCartList } from "../../features/cart/cartSlice";
+import { getCartList, toggleAllItems } from "../../features/cart/cartSlice";
 
 const CartPage = () => {
-  const { cartList, totalPrice } = useSelector((state) => state.cart);
+  const { cartList, selectedCartItems } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,13 +15,44 @@ const CartPage = () => {
     dispatch(getCartList());
   }, [dispatch]);
 
+  const selectedItems = useMemo(() => {
+    return cartList.filter((item) => selectedCartItems.includes(item._id));
+  }, [cartList, selectedCartItems]);
+
+  const selectedItemsTotalPrice = useMemo(() => {
+    return selectedItems.reduce(
+      (total, item) => total + item.productId.price * item.qty,
+      0
+    );
+  }, [selectedItems]);
+
+  const handleAllCheckboxChange = (e) => {
+    dispatch(toggleAllItems({ checked: e.target.checked }));
+  };
+
   return (
     <Container>
       <Row>
         <Col xs={12} md={7}>
+          <div className="flex items-center gap-2 mb-3">
+            <input
+              type="checkbox"
+              checked={
+                selectedCartItems.length === cartList.length &&
+                cartList.length > 0
+              }
+              onChange={handleAllCheckboxChange}
+              className="checkbox-large"
+            />
+            <span className="font-suit-600">전체 선택</span>
+          </div>
           {cartList.length > 0 ? (
             cartList.map((item) => (
-              <CartProductCard item={item} key={item._id} />
+              <CartProductCard
+                key={item._id}
+                item={item}
+                isChecked={selectedCartItems.includes(item._id)}
+              />
             ))
           ) : (
             <div className="text-align-center empty-bag">
@@ -31,7 +62,10 @@ const CartPage = () => {
           )}
         </Col>
         <Col xs={12} md={5}>
-          <OrderReceipt cartList={cartList} totalPrice={totalPrice} />
+          <OrderReceipt
+            cartList={selectedItems}
+            totalPrice={selectedItemsTotalPrice}
+          />
         </Col>
       </Row>
     </Container>
